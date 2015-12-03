22011a82c7cb395a939191c5a766eadb096dd39b
Sayonne <morgane.minodier@y-nov.com>
2015-12-03 20:27:09 +0100

    for pull

--- Key2Security/view/layout/default.php
+++ Key2Security/view/layout/default.php
@@ -23,7 +23,7 @@
 		    <!-- Collect the nav links, forms, and other content for toggling -->
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		      	<ul class="nav navbar-nav">
-			    	<li><a href="<?php echo Router::url('posts/index'); ?>">Blog</a></li>
+			    	<li><a href="<?php echo Router::url('posts/index'); ?>">Cours</a></li>
 			    	<li><a href="#">Rapport</a></li>
 		      	</ul>
 		      	<ul class="nav navbar-nav navbar-right">
